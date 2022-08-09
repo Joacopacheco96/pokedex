77b101aa5ac22c0220dba2e680ef735c4042d791
Natalia-Figueroa <nataliaafigueroa@gmail.com>
2022-08-09 20:57:59 -0300

    pruebas
    pruebas

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React dfasdfas
+          React
         </a>
       </header>
     </div>
