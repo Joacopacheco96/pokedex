16221500ed154a9933fcb0f085e244546952e9a2
Joacopacheco96 <97853299+Joacopacheco96@users.noreply.github.com>
2022-08-09 23:05:25 -0300

    agrego DB - posible Index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Index from './Components/Index/Index.jsx'
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+
+  <Index/>
+
+  </div>;
 }
 
 export default App;
